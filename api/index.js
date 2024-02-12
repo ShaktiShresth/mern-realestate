e56760616ab8eae85486dbc323c3a9ed59e21856
Shakti Shrestha <75934459+ShaktiShresth@users.noreply.github.com>
2024-02-12 14:08:30 +0545

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to the MongoDB of real estate app.");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
